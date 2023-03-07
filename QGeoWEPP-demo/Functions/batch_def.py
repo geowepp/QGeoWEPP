73833c46a689eea1b72ec9f11699f2f8ba495ce0
HanZhangGeo <111670590+HanZhangGeo@users.noreply.github.com>
2023-03-07 18:58:58 -0500

    Update batch_def.py

--- QGeoWEPP-demo/Functions/batch_def.py
+++ QGeoWEPP-demo/Functions/batch_def.py
@@ -6,7 +6,7 @@ Batch Processing for Soil Redistribution
                               -------------------
         begin                : 2021-06-11
         git sha              : $Format:%H$
-        author               : (C) 2022 by Han Zhang, Chris S. Renschler
+        author               : (C) 2022 by Han Zhang
         email                : support@geowepp.org
  ***************************************************************************/
 
