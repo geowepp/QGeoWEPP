c8d75a9058cafa217ce746135ff744976a0c2660
HanZhangGeo <111670590+HanZhangGeo@users.noreply.github.com>
2023-04-07 15:52:17 -0400

    Update qgeowepp_dialog.py

--- QGeoWEPP-demo/qgeowepp_dialog.py
+++ QGeoWEPP-demo/qgeowepp_dialog.py
@@ -31,11 +31,5 @@ FORM_CLASS, _ = uic.loadUiType(os.path.join(
 
 class QGeoWEPPDialog(QDialog, FORM_CLASS):
     def __init__(self, parent=None):
-        """Constructor."""
         super(QGeoWEPPDialog, self).__init__(parent)
-        # Set up the user interface from Designer.
-        # After setupUI you can access any designer object by doing
-        # self.<objectname>, and you can use autoconnect slots - see
-        # http://qt-project.org/doc/qt-4.8/designer-using-a-ui-file.html
-        # #widgets-and-dialogs-with-auto-connect
         self.setupUi(self)
